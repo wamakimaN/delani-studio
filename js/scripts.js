1e9a398c1d5ed54e3450272d151842a72dc79e68
wamakimaN <wamamoses@gmail.com>
2019-08-25 16:44:29 +0300

    make the description on 'what we do' appear and disappear when the icons are clicked

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".clickable").click(function() {
+    $(".hide").fadeToggle("slow");
+  })
+  $(".clickable1").click(function() {
+    $(".hide1").fadeToggle("slow");
+  })
+  $(".clickable2").click(function() {
+    $(".hide2").fadeToggle("slow");
+})
+})
