2b8ac75a69f8c7600425b17c2d68e91c9f054bba
wamakimaN <wamamoses@gmail.com>
2019-08-25 18:04:40 +0300

    make icons disappear when clicked and re-appear when the newly revealed text is clicked

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 $(document).ready(function(){
   $(".clickable").click(function() {
-    $(".hide").fadeToggle("slow");
+    $("#hide").slideToggle("slow");
+    $("#icon").slideToggle("slow");
   })
   $(".clickable1").click(function() {
-    $(".hide1").fadeToggle("slow");
+    $("#hide1").fadeToggle("slow");
+    $("#icon1").slideToggle("slow");
   })
   $(".clickable2").click(function() {
-    $(".hide2").fadeToggle("slow");
+    $("#hide2").fadeToggle("slow");
+    $("#icon2").slideToggle("slow");
 })
 })
