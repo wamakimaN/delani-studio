a69021aa6be9a315d2ca1bd6c3ddd4f4cd779481
wamakimaN <wamamoses@gmail.com>
2019-08-26 14:59:33 +0300

    validate name in contact form

--- js/scripts.js
+++ js/scripts.js
@@ -4,16 +4,50 @@ $(document).ready(function(){
     $("#icon").slideToggle("slow");
   })
   $(".clickable1").click(function() {
-    $("#hide1").fadeToggle("slow");
+    $("#hide1").slideToggle("slow");
     $("#icon1").slideToggle("slow");
   })
   $(".clickable2").click(function() {
-    $("#hide2").fadeToggle("slow");
-    $("#icon2").slideToggle("slow");
+   $("#hide2").slideToggle("slow");
+   $("#icon2").slideToggle("slow");
   })
   $(".portfolio img").hover(function() {
     $(this).animate({opacity:.5})
   }, function(){
     $(this).stop().animate({opacity:1})
   })
+
+  $(function() {
+    $("name-error").hide();
+    $("mail-error").hide();
+    $("message-error").hide();
+
+    var error_name = false;
+    var error_mail = false;
+    var error_message = false;
+
+  $("#name").focusout(function(){
+      check_name();
+    })
+  $("#mail").focusout(function(){
+      check_mail();
+    })
+  $("#message").focusout(function(){
+      check_message();
+    })
+    function check_name()  {
+      var pattern = /^[a-zA-Z]*$/;
+      var name = $("#name").val();
+      if(pattern.test(name) && name !== ''){
+        $("#name-error").hide();
+        $("#name").css("border-bottom","2px solid #34F458");
+      }else {
+        $("#name-error").html("Incorrect name format");
+        $("#name-eror").show();
+        $("#name").css("border-bottom","2px solid #F90A0A");
+        error_name = true;
+      }
+    }
+  
+})
 })
